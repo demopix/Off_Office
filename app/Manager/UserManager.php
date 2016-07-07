634295894f49245fd2c3ac125b448e3e7811e671
Alex8874 <weissflorian08@gmail.com>
2016-07-07 14:26:45 +0200

    managers

--- app/Manager/UserManager.php
+++ app/Manager/UserManager.php
@@ -7,7 +7,7 @@ class UsersManager extends \W\Manager\Manager
 	public function __construct(){
 		parent:: __construct();
 		//defintion du nom de la table
-		$this->setTable();
+		$this->setTable('Users');
 	}
 
 }
