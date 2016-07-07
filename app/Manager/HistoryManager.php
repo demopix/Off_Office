12ef146b66d20c4ba183037928843f0af3a0666c
Alex8874 <weissflorian08@gmail.com>
2016-07-07 11:32:49 +0200

    Added Managers + App/Config

---
+++ app/Manager/HistoryManager.php
@@ -0,0 +1,14 @@
+
+<?php 
+namespace Manager;
+
+class HistoryManager extends \W\Manager\Manager
+{
+	public function __construct(){
+		parent:: __construct();
+		//defintion du nom de la table
+		$this->setTable();
+	}
+
+}
+
