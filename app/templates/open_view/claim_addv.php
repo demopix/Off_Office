dd0327023f4ab2614785313a1c6a8ff41649be31
Alex8874 <weissflorian08@gmail.com>
2016-07-08 16:49:26 +0200

    upload function (#6)

--- app/templates/open_view/claim_addv.php
+++ app/templates/open_view/claim_addv.php
@@ -1,6 +1,12 @@
 <?php $this->layout('layout', ['title' => 'Sinistres !']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Sinistres.</h2>
-	
+	<h1>Upload a file</h1>
+
+<form method="POST" action="" enctype="multipart/form-data">
+     <!-- On limite le fichier à 100000Ko -->
+     <input type="hidden" name="MAX_FILE_SIZE" value="10000000">
+     Fichier : <input type="file" name="avatar">
+     <input type="submit" name="envoyer" value="Envoyer le fichier">
+</form>
 <?php $this->stop('main_content') ?>
