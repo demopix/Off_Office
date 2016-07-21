8e21ad115c7670eac0fd43a9775fd6f975faa4bc
Alex8874 <weissflorian08@gmail.com>
2016-07-21 16:47:16 +0200

    modif c details

--- app/templates/open_view/c_detailsv.php
+++ app/templates/open_view/c_detailsv.php
@@ -2,14 +2,14 @@
 
 <?php $this->start('main_content')?>
 
-<?php 	
+<?php   
 $id = $verify ['id'];
 $fname = $verify ['user_fname'];
 $lname = $verify ['user_lname'];
 $userAdressP = $verify ['user_address'];
 $userGen = $verify ['user_gender'];
 $contractId = $verify ['contract_id'];
-$cityId = $verify ['city_id'];
+$cityName = $verify ['city_name'];
 $countryId = $verify ['country_id'];
 $userBDate = $verify ['user_bdate'];
 $userPass = $verify ['user_password'];
@@ -26,7 +26,7 @@ $userMail = $verify ['user_email'];
     <tr>
      <td><label class="control-label">Nom de famille</label></td>
         <td><input class="form-control" type="text" name="user_lname" placeholder="<?php echo $verify ['user_lname']; ?>" />
-       	</td>
+        </td>
     </tr>
     <tr>
     <td><label class="control-label">Prénom</label></td>
@@ -35,19 +35,10 @@ $userMail = $verify ['user_email'];
     <td><label class="control-label">Adresse</label></td>
         <td><input class="form-control" type="text" name="user_address" placeholder="<?php echo $verify ['user_address']; ?>" /></td>
     </tr>
-	    <td><label class="control-label">Region</label></td>
-	    <td><input class="form-control" type="text" name="city_id" placeholder="<?php echo $verify ['city_id']; ?>" /><form method="" action="">
-    <select name="regions">
-        <option value="" selected="selected"></option>
-        <option value="1">Luxembourg </option>
-        <option value="2">Grevenmacher</option>
-        <option value="3">Diekirch</option>
-    </select>
-    <input TYPE="submit" name="submit" />
-</form>
-</td>
+      <td><label class="control-label">Ville</label></td>
+      <td><input class="form-control" type="text" name="city_name" placeholder="<?php echo $verify ['city_name']; ?>" />
+    </td>
     </tr>
-
     <td><label class="control-label">Sexe</label></td>
         <td><input class="form-control" type="text" name="user_gender" placeholder="<?php echo $verify ['user_gender']; ?>" /></td>
     </tr>
@@ -57,7 +48,7 @@ $userMail = $verify ['user_email'];
     <td><label class="control-label">Numéro téléphone/GSM</label></td>
         <td><input class="form-control" type="text" name="user_tel"placeholder="<?php echo $verify ['user_tel']; ?>" /></td>
     </tr>
-    <td><label class="control-label">Date inscription</label></td>verify
+    <td><label class="control-label">Date inscription</label></td>
         <td><input class="form-control" type="text" name="date_registry" placeholder="<?php echo $verify ['date_registry']; ?>" /></td>
     </tr>
     <td><label class="control-label">E-mail</label></td>
@@ -81,17 +72,39 @@ $userMail = $verify ['user_email'];
   <input placeholder="verifier mot de passe" type="password" name="" value="">
   <br><br>
   <input class="btn btn-success" type="submit" value="Update">
-</form>
 
-<div class="fluid-container"> -->
+
+</form>
+  
+      <form method="" action="">
+      
+          <select name="region">
+            <option value="" selected="selected"></option>
+            <option value="1">Capellen </option>
+            <option value="2">Clervaux</option>
+            <option value="3">Diekirch</option>
+            <option value="3">Echternach</option>
+            <option value="3">Esch-sur-Alzette</option>
+            <option value="3">Grevenmacher</option>
+            <option value="3">Luxembourg</option>
+            <option value="3">Mersch</option>
+            <option value="3">Redange</option>
+            <option value="3">Remich</option>
+            <option value="3">Vianden</option>
+            <option value="3">Wiltz</option>
+         </select>
+       <input TYPE="submit" name="submit" />
+      </form>
+
 
 <!--<h2>list of all docs refering to this client </h2>
-	<p>create form with place holders database rows<br>
-	update ask for password before submition   <br>
-	important! 2 rows  password <br>
-	if get session employ or admin don't show password row </p>-->
-	<br><br>
-	if login = client rule button = <a class="btn btn-warning" href="<?=$this->url("open_e_client");?>"> < Espace de Client</a><br><br>
+  <p>create form with place holders database rows<br>
+  update ask for password before submition   <br>
+  important! 2 rows  password <br>
+  if get session employ or admin don't show password row </p>-->
+  <br><br>
+  if login = client rule button = <a class="btn btn-warning" href="<?=$this->url("open_e_client");?>"> < Espace de Client</a><br><br>
 
-	if login = employee or admin rule button = <a class="btn btn-info" href="<?=$this->url("backoffice_main");?>">back office ></a>
+  if login = employee or admin rule button = <a class="btn btn-info" href="<?=$this->url("backoffice_main");?>">back office ></a>
 <?php $this->stop('main_content') ?>
+
