3a0b4051a3fb88dc16141177320f2db33c3a6faf
demopix <vicentedemterio@gmail.com>
2016-07-25 12:57:13 +0200

    ajour configAdmin

---
+++ app/configAdmin.php
@@ -0,0 +1,25 @@
+<?php 
+
+$w_config = [
+	//url
+	'base_url' => '/amy/Off_Office/backoffice',	//chemin relatif au domaine, menant à la racine de l'appli
+
+   	//information de connexion à la bdd
+	'db_host' => 'localhost',						//hôte (ip, domaine) de la bdd
+    'db_user' => 'root',							//nom d'utilisateur pour la bdd
+    'db_pass' => 'root',								//mot de passe de la bdd
+    'db_name' => 'offofficebd',								//nom de la bdd
+    'db_table_prefix' => '',						//préfixe ajouté aux noms de table
+
+	//authentification, autorisation
+	'security_user_table' => 'admin',				//nom de la table contenant les infos des utilisateurs
+	'security_id_property' => 'id',					//nom de la colonne pour la clef primaire
+	'security_username_property' => 'employ_name',		//nom de la colonne pour le "pseudo"
+	'security_email_property' => 'employ_email',			//nom de la colonne pour l'"email"
+	'security_password_property' => 'employ_password',		//nom de la colonne pour le "mot de passe"
+	'security_role_property' => 'role',				//nom de la colonne pour le "role"
+
+	'security_login_route_name' => 'admin_login',			//nom de la route affichant le formulaire de connexion
+];
+
+require('routes.php');
