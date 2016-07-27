daf8f12f882be3e62e5490b52e0e5f96a34cd150
demopix <vicentedemterio@gmail.com>
2016-07-27 14:27:12 +0200

    one more dprcted

--- app/templates/backoffice_view/e_loginv.php
+++
@@ -1,6 +0,0 @@
-<?php $this->layout('layout', ['title' => 'Back Office Login | Oo !']) ?>
-
-<?php $this->start('main_content') ?>
-	<h2>Back Office Login  .</h2>
-	
-<?php $this->stop('main_content') ?>
